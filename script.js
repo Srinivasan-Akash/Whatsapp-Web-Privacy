376d4621b0dcccdbdc9867c4d54e56f4fedb7ef2
Srinivasan-Akash <prashanthiakash@gmail.com>
2022-12-09 23:21:59 +0530

    code to listen for whatsap web.com and wait for it to completely load

---
+++ script.js
@@ -0,0 +1,7 @@
+const interval = setInterval(() => {
+    const whatsapp_first_contact = document.querySelector(".ggj6brxn.gfz4du6o.r7fjleex.g0rxnol2.lhj4utae.le5p0ye3.l7jjieqr.i0jNr")
+    if(whatsapp_first_contact !== null) {
+        alert("pageLoaded")
+        clearInterval(interval)
+    }
+}, 200)
